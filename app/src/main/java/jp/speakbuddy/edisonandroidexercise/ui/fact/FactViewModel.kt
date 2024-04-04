064bc89cd8492b434a45fa011b440b021592fd54
oh-naoki <oh.naoki0819@gmail.com>
2024-04-04 09:43:26 +0900

    feature: introduce Dagger Hilt

--- app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/FactViewModel.kt
+++ app/src/main/java/jp/speakbuddy/edisonandroidexercise/ui/fact/FactViewModel.kt
@@ -1,16 +1,18 @@
 package jp.speakbuddy.edisonandroidexercise.ui.fact
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.speakbuddy.edisonandroidexercise.network.FactServiceProvider
 import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
 
-class FactViewModel : ViewModel() {
-    fun updateFact(completion: () -> Unit): String =
-        runBlocking {
-            try {
-                FactServiceProvider.provide().getFact().fact
-            } catch (e: Throwable) {
-                "something went wrong. error = ${e.message}"
-            }.also { completion() }
-        }
+@HiltViewModel
+class FactViewModel @Inject constructor() : ViewModel() {
+    fun updateFact(completion: () -> Unit): String = runBlocking {
+        try {
+            FactServiceProvider.provide().getFact().fact
+        } catch (e: Throwable) {
+            "something went wrong. error = ${e.message}"
+        }.also { completion() }
+    }
 }
